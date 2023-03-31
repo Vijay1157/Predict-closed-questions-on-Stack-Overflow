826848c5881086713147a7fd2666ef5230d21565
Vijay1157 <95044120+Vijay1157@users.noreply.github.com>
2023-03-31 18:37:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from flask import Flask,render_template,request,g
 import re
 import numpy as np
 from tqdm.auto import tqdm
-
 import transformers
 import tensorflow
 from transformers import DistilBertTokenizer, TFDistilBertModel
-
 
 app=Flask(__name__)
 
@@ -47,6 +45,8 @@ def remove_underscore(text):
 
 def filter_words(text):
     return ' '.join(word for word in text.split() if len(word) > 1)
+
+
 @app.route("/")
 def welcome():
     return render_template("index.html")
@@ -61,17 +61,14 @@ def run2():
     a = filter_words(a)
     processed_data = prepare_data(a, tokenizer)
     result = make_prediction(model, processed_data=processed_data)
-    if result == "HQ" or result=="LQ_EDIT":
-        
+    if result == "HQ" or result=="LQ_EDIT":   
         data="This question is valid and will be accessable in Stack Overflow"
     else:
         data="This question is not valid and will be closed soon!"
         
-    
     
     return render_template("index.html",new=data)
 
 
 if __name__ == '__main__':
     app.run(debug=False)
-
